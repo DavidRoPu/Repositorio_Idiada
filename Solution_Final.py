90124e65e46442ac39f51e33c74378147f05e506
David_Romero_Puyal <davidrompuy99@gmail.com>
2022-05-18 13:37:35 +0200

    Rover class deleted, change main program with while

--- Solution_Final.py
+++ Solution_Final.py
@@ -1,5 +1,8 @@
 # Coding Problem Solution Idiada David Romero Puyal
 
+nrovers = 2
+actual_rover = 0
+
 
 # Plateu class to save the limits of the plateau grid's.
 class Plateau:
@@ -8,20 +11,6 @@ class Plateau:
         self.ymin = 0
         self.xmax = xmaxplateau
         self.ymax = ymaxplateau
-
-
-# Rover class to create as many rover objects as rovers are necessary.
-# rover_position_in is made to save the initial position of the rover.
-# rover_position_in is made to operate.
-# rover_position_out is made to save the final position of the rover.
-# rover_position_instructions is made to save the instructions of the rover.
-class Rover:
-    def __init__(self, rpin, rp, rpout, rins):
-        self.rover_position_in = rpin
-        self.rover_position = rp
-        self.rover_position_out = rpout
-        self.rover_position_instruction = rins
-        self.rover_error = 0
 
 
 # The following function allows to rotate the rover to the left.
@@ -104,77 +93,55 @@ def move(rover_position):
         rover_position = ''.join(lst)
     return rover_position
 
+# Open the file and save the first line (Plateau size).
+with open("data", "r") as fname:
+    size_plateau = fname.readline().rstrip()
+    # It is necessary to ensure that the rover does not go off the grid.
+    plateau = Plateau(int(size_plateau[0]), int(size_plateau[2]))
 
-# Indicate the size of the Plateau.
-# It is necessary to ensure that the rover does not go off the grid.
-size_plateau = input("Upper-Right Coordinates of the Plateau: ")
-plateau = Plateau(int(size_plateau[0]), int(size_plateau[2]))
+# Loop to analyze the movement of the rover
+    while actual_rover < nrovers:
 
-# ROVER 1. Initial Position and Instructions.
-# ROVER 1 Object creation.
-roposin = input("Rover 1 Initial Position: ")
-roins = input("Rover 1 Instructions: ")
-rover1 = Rover(roposin, roposin, roposin, roins)
+        rover_position_in = fname.readline().rstrip()
+        rover_instruction_in = fname.readline().rstrip()
 
-# ROVER 2. Initial Position and Instructions
-# ROVER 2 Object creation.
-roposin = input("Rover 2 Initial Position: ")
-roins = input("Rover 2 Instructions: ")
-rover2 = Rover(roposin, roposin, roposin, roins)
+# Save the values in new variable to operate.
+        actual_rover_position = rover_position_in
+        actual_rover_instruction = rover_instruction_in
+        rover_error = 0
+# In case of error, rover_error change the state and prevents the result of being printed.
+# Instead of writting the result in the file, the error is written
 
-
-# ROVER 1. Execution of the instructions.
-# For loop to make the rover 1 instructions. The for loop iterates the number of statements unless a statement is wrong.
+# Execution of the instructions.
+# For loop to make the rover instructions. The for loop iterates the number of statements unless a statement is wrong.
 # If a statement is wrong the program exits the loop and print error.
-# M --> Move function.
-# L --> Rotate left.
-# R --> Rotate right.
-for index in range(len(rover1.rover_position_instruction)):
-    if rover1.rover_position_instruction[index] == "M":
-        rover1.rover_position = move(rover1.rover_position)
-        if rover1.rover_position == 'X':
-            rover1.rover_error = 1
-            print("ERROR, ROVER 1 POSITION OUT OF RANGE")
-            break
-    elif rover1.rover_position_instruction[index] == "L":
-        rover1.rover_position = rotate_left(rover1.rover_position)
-    elif rover1.rover_position_instruction[index] == "R":
-        rover1.rover_position = rotate_right(rover1.rover_position)
-    else:
-        rover1.rover_error = 1
-        print("ERROR, NOT VALID ROVER 1 INSTRUCTION")
-        break
+        for index in range(len(actual_rover_instruction)):
+            if actual_rover_instruction[index] == "M":                          # M --> Move function.
+                actual_rover_position = move(actual_rover_position)
+                if actual_rover_position == 'X':
+                    rover_error = 1
+                    print("ERROR, ROVER 1 POSITION OUT OF RANGE")
+                    break
+            elif actual_rover_instruction[index] == "L":
+                actual_rover_position = rotate_left(actual_rover_position)      # L --> Rotate left.
+            elif actual_rover_instruction[index] == "R":
+                actual_rover_position = rotate_right(actual_rover_position)     # R --> Rotate right.
+            else:
+                rover_error = 1     #Invalid command
+                print("ERROR, NOT VALID ROVER 1 INSTRUCTION")
+                break
 
-# ROVER 1. If not error, print the final position.
-if rover1.rover_error == 0:
-    rover1.rover_position_out = rover1.rover_position
-    print(rover1.rover_position_out)
-rover1.rover_error = 0
+        # If not error, print the final position.
+        if rover_error == 0:
+            rover_position_out = actual_rover_position
+            with open('solution', 'a') as fsol:
+                fsol.write(rover_position_out)
+                fsol.write('\n')
+            print(rover_position_out)
 
-# ROVER 2. Execution of the instructions.
-# For loop to make the rover 2 instructions. The for loop iterates the number of statements unless a statement is wrong.
-# If a statement is wrong the program exits the loop and print error.
-# M --> Move function.
-# L --> Rotate left.
-# R --> Rotate right.
-for index in range(len(rover2.rover_position_instruction)):
-    if rover2.rover_position_instruction[index] == "M":
-        rover2.rover_position = move(rover2.rover_position)
-        if rover2.rover_position == 'X':
-            rover2.rover_error = 1
-            print("ERROR, ROVER 2 POSITION OUT OF RANGE")
-            break
-    elif rover2.rover_position_instruction[index] == "L":
-        rover2.rover_position = rotate_left(rover2.rover_position)
-    elif rover2.rover_position_instruction[index] == "R":
-        rover2.rover_position = rotate_right(rover2.rover_position)
-    else:
-        rover2.rover_error = 1
-        print("ERROR, NOT VALID ROVER 2 INSTRUCTION")
-        break
+        actual_rover = actual_rover + 1
 
-# ROVER 2. If not error, print the final position.
-if rover2.rover_error == 0:
-    rover2.rover_position_out = rover2.rover_position
-    print(rover2.rover_position_out)
-rover2.rover_error = 0
+
+# Things to change.
+    # Position enters as 2 integers and 1 character.
+    # Optimize functions.
---
+++ Tests.py
@@ -0,0 +1,26 @@
+#Tests for read file
+
+with open('data', "r") as fdname:
+        nrov = len(fdname.readlines())
+        print(nrov, '\n')
+
+with open('data', "r") as fdname:
+        i = 0
+        while i < nrov:
+                print(fdname.readline())
+                i+=1
+
+
+
+#Tests for write file
+
+with open('solution', "w") as fsname:
+        fsname.write('OUTPUT\n')
+
+with open('data', "r") as fdname:
+    nrov = len(fdname.readlines())
+    i = 0
+    while i < nrov:
+        with open('solution', "a") as fsname:
+                fsname.write("%i\n" % (i))
+                i+=1
